29f81f13bce0afe95e77999c4a82727230e8d656
Pratik Tambe <enthusiasticgeek@gmail.com>
2023-11-20 19:10:09 -0500

    comment added to script

--- src/test_pipeline.sh
+++ src/test_pipeline.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#Change this to match the rtsp server ip address.
 IP=192.168.1.5
 /usr/bin/gst-launch-1.0 -v rtspsrc location="rtsp://${IP}:8554/test" latency=0 ! decodebin ! videoconvert ! autovideosink
