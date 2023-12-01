363719f3a5a6ac3f91508f3d55b6e6696a53079e
enthusiastic geek <enthusiasticgeek@gmail.com>
2023-12-01 12:44:04 -0500

    Update tcp_client.sh

--- src/tcp_client.sh
+++ src/tcp_client.sh
@@ -3,5 +3,5 @@ HOST=0.0.0.0
 PORT=7001
 /usr/bin/gst-launch-1.0 tcpclientsrc host=${HOST} port=${PORT}  \
     ! matroskademux \
-    ! jpegdec \
+    ! jpegdec ! videoconvert ! gdkpixbufoverlay location=image.png ! clockoverlay time-format="%e-%h-%G %r" shaded-background=false  font-desc="Sans 10"  \
     ! autovideosink sync=false
